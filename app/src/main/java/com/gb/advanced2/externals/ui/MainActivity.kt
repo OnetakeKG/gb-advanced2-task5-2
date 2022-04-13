9c0a99d3f66e6f7b8b4c408e64d857fc21370efb
user <gufeld1488@gmail.com>
2022-04-13 11:58:28 +0300

    commit 13/04

--- app/src/main/java/com/gb/advanced2/externals/ui/MainActivity.kt
+++ app/src/main/java/com/gb/advanced2/externals/ui/MainActivity.kt
@@ -17,6 +17,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+        Timber.d("!! helper ${SomeHelper().doSomething(3, 5)}")
     }
 
     override fun onResumeFragments() {
